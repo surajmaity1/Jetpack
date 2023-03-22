21f369e8a1471bb7a02b25a0e3740037b67e36fb
surajmaity1 <surajmaity981@gmail.com>
2023-03-22 10:08:31 +0530

    State (Part 1) - Jetpack Compose

--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -4,64 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val fontFamily = FontFamily(
-            Font(
-                R.font.delicious_handrawn_regular,
-                FontWeight.Medium
-            )
-        )
-
         setContent{
-            Box(modifier = Modifier.fillMaxSize()
-                .background(Color(0xFFD4CA8E))
-            ){
-                Text(
-                    text = buildAnnotatedString{
-                           withStyle(
-                               style = SpanStyle(
-                                   color = Color.White,
-                                   fontSize = 60.sp
-                               )
-                           ){
-                               append("H")
-                           }
-                           append("ello")
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Color.White,
-                                    fontSize = 60.sp
-                                )
-                            ){
-                                append("E")
-                            }
-                        append("veryone :)")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic
-                )
-            }
+            ColorBox(Modifier.fillMaxSize())
         }
     }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier
+){
+    val color = remember{
+        mutableStateOf(Color.Blue)
+    }
+
+    Box(
+        modifier = modifier
+            .background(color.value)
+            .clickable {
+                color.value = Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            }
+    )
 }

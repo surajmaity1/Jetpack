4eea4b04799e104fa1dcc5003ad101cbd438b1ce
surajmaity1 <surajmaity981@gmail.com>
2023-03-22 11:28:13 +0530

    List with Vertical Scroll -Jetpack Compose

--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -3,59 +3,35 @@ package com.sun.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
-            val scaffoldState = rememberScaffoldState()
-            
-            var textFieldState by remember{
-                mutableStateOf("")
-            }
-
-            val scope = rememberCoroutineScope()
-
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 25.dp)
-                ) {
-                    TextField(
-                        value = textFieldState,
-                        onValueChange = { textFieldState = it },
-                        label = { Text(text = "Enter Name")},
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
+        setContent {
+            val scrollState = rememberScrollState()
+            Column(modifier = Modifier.verticalScroll(scrollState)) {
+                for (i in 1..200){
+                    Text(
+                        text = "Item $i",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 10.dp)
                     )
-                    Spacer(modifier = Modifier.height(14.dp))
-                    Button(onClick = {
-                        /*
-                        Note: Never launch a coroutine directly in the composable,
-                         it's only okay in callbacks such as an onClickListener
-                         */
-                        scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState, Have a nice day!")
-                        }
-                    }) {
-                        Text(text = "Show SnackBar")
-                    }
                 }
-
             }
         }
     }

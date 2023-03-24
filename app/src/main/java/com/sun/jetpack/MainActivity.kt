ebf343fcd909f0e9ebe0d27ad195de6cc984e504
suraj <surajmaity981@gmail.com>
2023-03-24 09:46:27 +0530

    ConstraintLayout 1 - Jetpack Compose

--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -3,35 +3,45 @@ package com.sun.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn() {
-                itemsIndexed(
-                    listOf("1", "2", "3", "4")
-                ){ idx, str ->
-                    Text(
-                        text = str,
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 10.dp)
-                    )
+            val constraintSet = ConstraintSet {
+                val blueBox = createRefFor("bluebox")
+                val blackBox = createRefFor("blackbox")
+
+                constrain(blueBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    height = Dimension.value(150.dp)
+                    width = Dimension.value(150.dp)
                 }
+                constrain(blackBox) {
+                    top.linkTo(blueBox.bottom)
+                    start.linkTo(parent.start)
+                    height = Dimension.value(150.dp)
+                    width = Dimension.value(150.dp)
+                }
+            }
+            ConstraintLayout(constraintSet = constraintSet, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier
+                    .background(Color.Blue)
+                    .layoutId("bluebox"))
+                Box(modifier = Modifier
+                    .background(Color.Black)
+                    .layoutId("blackbox"))
             }
         }
     }

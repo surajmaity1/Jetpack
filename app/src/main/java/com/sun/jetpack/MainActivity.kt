1a5c666eb1deacfb83929538e373f41830842f13
suraj <surajmaity981@gmail.com>
2023-03-25 16:21:50 +0530

    Coroutine 1

--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -1,54 +1,26 @@
 package com.sun.jetpack
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
+import androidx.compose.material.Text
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val TAG = "CoroutineTest"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val constraintSet = ConstraintSet {
-                val blueBox = createRefFor("bluebox")
-                val blackBox = createRefFor("blackbox")
-                val guidelineFromTop = createGuidelineFromTop(0.7f)
-
-                constrain(blueBox) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    height = Dimension.value(150.dp)
-                    width = Dimension.value(150.dp)
-                }
-                constrain(blackBox) {
-                    top.linkTo(guidelineFromTop)
-                    start.linkTo(parent.start)
-                    // end.linkTo(parent.end)
-
-                    height = Dimension.value(150.dp)
-                    width = Dimension.value(150.dp)
-                    // width = Dimension.fillToConstraints
-                }
-                createHorizontalChain(blueBox, blackBox, chainStyle = ChainStyle.SpreadInside)
-            }
-            ConstraintLayout(constraintSet = constraintSet, modifier = Modifier.fillMaxSize()) {
-                Box(modifier = Modifier
-                    .background(Color.Blue)
-                    .layoutId("bluebox"))
-                Box(modifier = Modifier
-                    .background(Color.Black)
-                    .layoutId("blackbox"))
-            }
+        setContent{
+            Text(text = "Coroutine Learning")
         }
+        GlobalScope.launch {
+            delay(5000L)
+            Log.d(TAG, "Coroutine wave Hello from ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Coroutine wave Hello from ${Thread.currentThread().name}")
     }
 }

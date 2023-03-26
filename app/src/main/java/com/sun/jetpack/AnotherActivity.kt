665626dbe00eb1073c2e1eb8594e5940870a5733
surajmaity1 <surajmaity981@gmail.com>
2023-03-26 19:29:35 +0530

    Coroutine - Before Using lifecycle and viewmodel scope

---
+++ app/src/main/java/com/sun/jetpack/AnotherActivity.kt
@@ -0,0 +1,32 @@
+package com.sun.jetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sun.jetpack.ui.theme.JetpackTheme
+
+class AnotherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "AnotherActivity")
+            }
+        }
+    }
+}
--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -1,46 +1,59 @@
 package com.sun.jetpack
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.style.AlignmentSpan
 import android.util.Log
 import androidx.activity.ComponentActivity
-import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private val TAG = "Coroutine Async and Await"
+    private val TAG = "Coroutine"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Note: Here first networkCall1 and then networkCall2 will execute
-        // We can check total time using measureTimeMillis{}
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = {
 
-        // To solve this, we'll launch each coroutine for each networkCall(), we'll using async
-        GlobalScope.launch(Dispatchers.IO) {
+                    // Note, as here we use GlobalScope, it will be still executing if we go to another actvity
+                    // There is memory leak
+                    GlobalScope.launch {
+                        while (true) {
+                            delay(1000L)
+                            Log.d(TAG, "Still running ...")
+                        }
+                    }
 
-            val totalTime = measureTimeMillis {
+                    GlobalScope.launch {
+                        delay(4000L)
 
-                val getResponse1 = async {
-                    demoNetworkCall1()
+                        val intent = Intent(this@MainActivity, AnotherActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                }) {
+                    Text(text = "Go To Another Activity")
                 }
-                val getResponse2 = async {
-                    demoNetworkCall2()
-                }
-
-                Log.d(TAG, "Response from network1: ${getResponse1.await()}")
-                Log.d(TAG, "Response from network2: ${getResponse2.await()}")
             }
-            Log.d(TAG, "Time Taken: $totalTime ms.")
+
         }
-    }
-
-    suspend fun demoNetworkCall1(): String {
-        delay(1000L)
-        return "answer from demoNetworkCall1"
-    }
-
-    suspend fun demoNetworkCall2(): String {
-        delay(1000L)
-        return "answer from demoNetworkCall2"
     }
 }

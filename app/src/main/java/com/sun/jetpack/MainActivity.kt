afcc20af68e445edce18d18958d6858b37a5b072
surajmaity1 <surajmaity981@gmail.com>
2023-03-26 19:34:25 +0530

    Coroutine - lifecycleScope{}

--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -32,9 +33,14 @@ class MainActivity : ComponentActivity() {
             ) {
                 Button(onClick = {
 
-                    // Note, as here we use GlobalScope, it will be still executing if we go to another actvity
+                    // Note, if we use GlobalScope, it will be still executing if we go to another actvity
                     // There is memory leak
-                    GlobalScope.launch {
+
+                    //So, for this reason, we'll use lifecyclescope, it will execute when we'll stay in the MainActivity
+                    // if our activity's lifecycle destroyed, all coroutine will be destroyed
+
+                    // Note: viewmodelScope is similar to lifecycleScope, it will be alive as long as viewmodel is alive
+                    lifecycleScope.launch {
                         while (true) {
                             delay(1000L)
                             Log.d(TAG, "Still running ...")

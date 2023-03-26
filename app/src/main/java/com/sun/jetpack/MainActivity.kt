e060d425db64c6e3d64b2d12bf2aba3943d3c391
surajmaity1 <surajmaity981@gmail.com>
2023-03-26 15:59:39 +0530

    Coroutine - Async

--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -3,10 +3,7 @@ package com.sun.jetpack
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +14,28 @@ class MainActivity : ComponentActivity() {
 
         // Note: Here first networkCall1 and then networkCall2 will execute
         // We can check total time using measureTimeMillis{}
-        GlobalScope.launch(Dispatchers.IO) {
-            val totalTime = measureTimeMillis {
-                val networkCall1 = demoNetworkCall1()
-                val networkCall2 = demoNetworkCall2()
 
-                Log.d(TAG, "Response from network1 ${demoNetworkCall1()}")
-                Log.d(TAG, "Response from network2 ${demoNetworkCall2()}")
+        // To solve this, we'll launch each coroutine for each networkCall,
+        // Note: It is not a good practice, for learning, we'll see how it works
+        // we'll implement alternative approach later.
+        GlobalScope.launch(Dispatchers.IO) {
+
+            val totalTime = measureTimeMillis {
+                var networkCall1: String? = null
+                var networkCall2: String? = null
+
+                val job1 = launch {
+                    networkCall1 = demoNetworkCall1()
+                }
+                val job2 = launch {
+                    networkCall2 = demoNetworkCall2()
+                }
+
+                job1.join()
+                job2.join()
+
+                Log.d(TAG, "Response from network1 $networkCall1")
+                Log.d(TAG, "Response from network2 $networkCall2")
             }
             Log.d(TAG, "Time Taken: $totalTime ms.")
         }

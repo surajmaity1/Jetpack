22911e69fa3de9d5837a45ddef904f8b5cc20ea7
surajmaity1 <surajmaity981@gmail.com>
2023-04-05 16:47:29 +0530

    Animated Circular Progress Bar - Jetpack

--- app/src/main/java/com/sun/jetpack/MainActivity.kt
+++ app/src/main/java/com/sun/jetpack/MainActivity.kt
@@ -3,20 +3,24 @@ package com.sun.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.*
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
@@ -24,66 +28,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var sizeState by remember {
-                mutableStateOf(100.dp)
-            }
-
-            val size by animateDpAsState(
-                targetValue = sizeState,
-                tween(
-                    durationMillis = 2000
-                )
-                /* 3.keyframes{} animation
-                keyframes {
-                    durationMillis = 5000
-                    sizeState at 0 with LinearEasing
-                    sizeState * 1.5f at 2500 with LinearOutSlowInEasing
-                    sizeState * 2f at 5000
-                }
-
-                 */
-
-                /*2. spring() animation
-                spring(
-                    dampingRatio = Spring.DampingRatioHighBouncy
-                )*/
-
-
-                /*1. tween () animation
-                tween(
-                    durationMillis = 2000,
-                    delayMillis = 200,
-                    easing = LinearOutSlowInEasing
-                )
-                */
-            )
-
-            // Color animation : One color to another color with reverse ...
-
-            val infiniteTransition = rememberInfiniteTransition()
-            val customizeColor by infiniteTransition.animateColor(
-                initialValue = Color.Cyan,
-                targetValue = Color.Blue,
-                animationSpec = infiniteRepeatable(
-                    tween(
-                        durationMillis = 1500
-                    ),
-                    repeatMode = RepeatMode.Reverse
-                )
-            )
-
             Box(
-                modifier = Modifier
-                    .background(customizeColor)
-                    .size(size),
-                contentAlignment = Alignment.Center
-            ){
-                Button(onClick = {
-                    sizeState += 40.dp
-                }) {
-                    Text(text = "Make Larger")
-                }
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CustomCircularProgressBar(percentage = 0.8f, num = 100)
             }
         }
+    }
+}
+
+@Composable
+fun CustomCircularProgressBar (
+    percentage: Float,
+    num: Int,
+    fontSize: TextUnit = 60.sp,
+    radius: Dp = 80.dp,
+    color: Color = Color.Red,
+    strokeWidth: Dp = 10.dp,
+    animeDuration: Int = 1000,
+    animeDelay: Int = 0
+) {
+    var isAnimationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val currentPercentage = animateFloatAsState(
+        targetValue = if (isAnimationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animeDuration,
+            delayMillis = animeDelay
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        isAnimationPlayed = true
+    }
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ){
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(
+                    strokeWidth.toPx(),
+                    cap = StrokeCap.Round
+                )
+            )
+        }
+        Text(
+            text = (currentPercentage.value * num).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 }

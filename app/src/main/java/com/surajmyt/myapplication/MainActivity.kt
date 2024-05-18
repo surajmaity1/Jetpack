a93ab3ea2e414188a58d092ee06e86a00c03437c
Suraj Maity <surajmaity981@gmail.com>
2024-05-18 14:11:29 +0530

    State
    State in Compose & State hoisting

--- app/src/main/java/com/surajmyt/myapplication/MainActivity.kt
+++ app/src/main/java/com/surajmyt/myapplication/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.surajmyt.myapplication
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -18,29 +26,54 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.surajmyt.myapplication.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
+            JetpackComposeTheme {
+                MyApp()
+            }
         }
     }
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("world !", "compose !")) {
+fun MyApp() {
+    var shouldShowOnBoarding by rememberSaveable {
+        mutableStateOf(true)
+    }
+
+    if (shouldShowOnBoarding) {
+        OnboardingScreen(onContinueClicked = {shouldShowOnBoarding = false})
+    }
+    else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = List(1000) {"I'm $it"}) {
     Surface(color = MaterialTheme.colorScheme.background) {
         Column(
             modifier = Modifier
                 .padding(vertical = 4.dp)
         ) {
-            for (name in names) {
-                Greeting(name = name)
+            LazyColumn {
+                items(names) {name ->
+                    Greeting(name = name)
+                }
             }
         }
     }
@@ -48,6 +81,16 @@ fun MyApp(names: List<String> = listOf("world !", "compose !")) {
 
 @Composable
 fun Greeting(name: String) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = tween(durationMillis = 300)
+    )
+
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
 
@@ -55,23 +98,47 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ },
+            OutlinedButton(onClick = { expanded = !expanded },
                 colors = ButtonDefaults
                     .outlinedButtonColors
                         (contentColor =  MaterialTheme.colorScheme.primary, containerColor = Color.White),
 
             ) {
-                Text(text = "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
 
     }
 }
 
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit
+) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+            ) {
+            Text(text = "Welcome to Basic CodeLab !")
+            Button(onClick = onContinueClicked, modifier = Modifier.padding(vertical = 24.dp)) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true, widthDp = 320, heightDp = 320)
+@Composable
+fun PreviewOnboardingScreen() {
+    OnboardingScreen(onContinueClicked = {})
+}
 @Preview(widthDp = 320)
 @Composable
 fun PreviewMessageCard() {
